beabd0aa1a5d2135e90cbebf55dddce19fc4c8d2
OptimusPrime <gulliverraed@gmail.com>
2025-11-25 15:25:01 +0200

    Initial Commit

---
+++ Fragrantica/FragranticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FragranticaApp.swift
+//  Fragrantica
+//
+//  Created by Gulliver Raed on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FragranticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
